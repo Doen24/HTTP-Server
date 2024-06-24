9d1a86a23acddae00362b1c8034066b5e81ec22e
Doen24 <1557399042@qq.com>
2024-06-24 22:43:40 +0800

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-
     let request_line=buf_reader.lines().next().unwrap().unwrap();
     //Resolusion 1  :按行解析，把status_line作为一个整体
     // let (status_line,filename)=if request_line=="GET / HTTP/1.1"{
@@ -24,24 +23,24 @@ fn handle_connection(mut stream: TcpStream) {
     // }else{
     //     ("HTTP/1.1 404 Not Found","404.html")
     // };
-
     //Resolusion 2  :按词解析，因为status_line是规则的，method+space+uri+space+httpversion+CR+LF
     let parts:Vec<&str>=request_line.split_whitespace().collect();
     if parts.len()==3{
         let method=parts[0];
         let uri=parts[1];
         let httpversion=parts[2];
-    }
+    
 
-    let parts2:Vec<&str>=uri.split("/").collect();
-    if parts2.len()==2{
-        let contents=parts2[1];
-        if parts2[0]=="echo" {
-            let status_line="HTTP/1.1 200 OK";
-            let length=contents.len();
-            let response=
-                format!("{status_line}\r\nContent-Type:text/plain\r\nContent-Length:{length}\r\n\r\n{contents}");
-            stream.write_all(response.as_bytes()).unwrap();    
+        let parts2:Vec<&str>=uri.split("/").collect();
+            if parts2.len()==2{
+            let contents=parts2[1];
+                if parts2[0]=="echo" {
+                let status_line="HTTP/1.1 200 OK";
+                let length=contents.len();
+                let response=
+                    format!("{status_line}\r\nContent-Type:text/plain\r\nContent-Length:{length}\r\n\r\n{contents}");
+                stream.write_all(response.as_bytes()).unwrap();    
+        }
         }
     }else{
         status_line="HTTP/1.1 404 Not Found";
