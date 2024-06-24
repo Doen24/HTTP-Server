2ad44a8e517051a99f3478395da82899d747032b
Doen24 <1557399042@qq.com>
2024-06-24 22:49:19 +0800

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -40,17 +40,14 @@ fn handle_connection(mut stream: TcpStream) {
                 let response=
                     format!("{status_line}\r\nContent-Type:text/plain\r\nContent-Length:{length}\r\n\r\n{contents}");
                 stream.write_all(response.as_bytes()).unwrap();    
-        }
-        }
+                }
+            }
     }else{
         // let status_line="HTTP/1.1 404 Not Found";
         // let response=format!("{status_line}\r\n\r\n");
-        stream.write_all("HTTP/1.1 404 Not Found".as_bytes()).unwrap();    
+        stream.write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes()).unwrap();    
     }
 
 
-    // let contents=fs::read_to_string(filename).unwrap();
-    // let length=contents.len();
-    // stream.write_all(response.as_bytes()).unwrap();    
-       
+    
 }
