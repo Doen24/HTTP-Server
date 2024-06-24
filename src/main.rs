4e0b99275c0d59e7a70d9c2b948fcc8440f0c402
Doen24 <1557399042@qq.com>
2024-06-24 22:51:05 +0800

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,8 @@ fn handle_connection(mut stream: TcpStream) {
 
         let parts2:Vec<&str>=uri.split("/").collect();
             if parts2.len()==2{
-            let contents=parts2[1];
-                if parts2[0]=="echo" {
+            let contents=parts2[2];
+                if parts2[1]=="echo" {
                 let status_line="HTTP/1.1 200 OK";
                 let length=contents.len();
                 let response=
