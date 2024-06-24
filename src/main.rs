8691d4e3f49c1b299cff0c1a413c26bd784a449d
Doen24 <1557399042@qq.com>
2024-06-24 21:24:24 +0800

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,12 @@ fn handle_connection(mut stream: TcpStream) {
     }else{
         ("HTTP/1.1 404 NOT FOUND","404.html")
     };
-    let contents=fs::read_to_string(filename).unwrap();
+    // let contents=fs::read_to_string(filename).unwrap();
     // let length=contents.len();
     let response=
         // format!("{status_line}\r\nContent-Length:{length}\r\n\r\n{contents}");
         format!("{status_line}\r\n\r\n{contents}");
+        format!("{status_line}\r\n\r\n");
     stream.write_all(response.as_bytes()).unwrap();    
        
 }
