fe4ae685106f0edb65e32ff393fe0ef9c69a8605
Doen24 <1557399042@qq.com>
2024-06-27 01:44:29 +0800

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ fn main() {
     }
 }
 
-fn handle_connection(mut stream: TcpStream,directory:String) {
+fn handle_connection(mut stream: TcpStream,directory:&str) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line=buf_reader.lines().map(|line|line.unwrap()).take_while(|line|!line.is_empty()).collect::<Vec<String>>();
 
